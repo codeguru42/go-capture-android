efbf6b3fa4f5aa96181d1cd74f035adc0a5b03d5
Code-Apprentice <codeguru42@gmail.com>
2022-10-23 20:06:14 -0600

    Add a button

--- app/src/main/java/codeguru/gocapture/MainActivity.kt
+++ app/src/main/java/codeguru/gocapture/MainActivity.kt
@@ -2,10 +2,17 @@ package codeguru.gocapture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun captureImage(view: View) {
+        Snackbar.make(view, "Button clicked", BaseTransientBottomBar.LENGTH_LONG).show()
     }
 }

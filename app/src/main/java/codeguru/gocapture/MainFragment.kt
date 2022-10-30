0b6814684f01191974f166980dca4f5ff3261fe4
Code-Apprentice <codeguru42@gmail.com>
2022-10-30 01:18:55 -0600

    Add button listener to select image

--- app/src/main/java/codeguru/gocapture/MainFragment.kt
+++ app/src/main/java/codeguru/gocapture/MainFragment.kt
@@ -15,9 +15,15 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
-        val button: ImageButton = view.findViewById(R.id.camera_button)
-        button.setOnClickListener {
+
+        val cameraButton: ImageButton = view.findViewById(R.id.camera_button)
+        cameraButton.setOnClickListener {
             captureImage()
+        }
+
+        val imageButton: ImageButton = view.findViewById(R.id.image_button)
+        imageButton.setOnClickListener {
+            loadImage()
         }
 
         // Inflate the layout for this fragment
@@ -26,6 +32,11 @@ class MainFragment : Fragment() {
 
     private fun captureImage() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivity(intent)
+    }
+
+    private fun loadImage() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
         startActivity(intent)
     }
 }

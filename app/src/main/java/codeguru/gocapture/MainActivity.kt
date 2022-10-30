a79e9d0f58583884cf8b3192860d4326beb4b84f
Code-Apprentice <codeguru42@gmail.com>
2022-10-29 19:55:57 -0600

    Add button to fragment

--- app/src/main/java/codeguru/gocapture/MainActivity.kt
+++ app/src/main/java/codeguru/gocapture/MainActivity.kt
@@ -1,21 +1,11 @@
 package codeguru.gocapture
 
-import android.content.Intent
 import android.os.Bundle
-import android.provider.MediaStore
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun captureImage(view: View) {
-        val intent = Intent().apply {
-            action = MediaStore.ACTION_IMAGE_CAPTURE
-        }
-        startActivity(intent)
     }
 }
--- app/src/main/java/codeguru/gocapture/MainFragment.kt
+++ app/src/main/java/codeguru/gocapture/MainFragment.kt
@@ -1,18 +1,34 @@
 package codeguru.gocapture
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
 class MainFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        val button: Button = view.findViewById(R.id.camera_button)
+        button.setOnClickListener {
+            captureImage(it)
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return view
+    }
+
+    private fun captureImage(view: View) {
+        val intent = Intent().apply {
+            action = MediaStore.ACTION_IMAGE_CAPTURE
+        }
+        startActivity(intent)
     }
 
     companion object {

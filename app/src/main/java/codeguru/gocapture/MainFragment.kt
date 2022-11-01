d3fea5ffcf55c45ea1a8e3a948c567fd1311db80
Code-Apprentice <codeguru42@gmail.com>
2022-10-31 23:45:30 -0600

    Use results api

--- app/src/main/java/codeguru/gocapture/MainFragment.kt
+++ app/src/main/java/codeguru/gocapture/MainFragment.kt
@@ -1,13 +1,17 @@
 package codeguru.gocapture
 
-import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
+import java.io.File
+
 
 class MainFragment : Fragment() {
     override fun onCreateView(
@@ -16,27 +20,34 @@ class MainFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
 
+        val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            Log.d(MainFragment::class.toString(), "get content result")
+            Log.d(MainFragment::class.toString(), uri.toString())
+        }
+
+        val takePicture = registerForActivityResult(ActivityResultContracts.TakePicture()) { saved: Boolean ->
+            Log.d(MainFragment::class.toString(), "take picture result")
+            Log.d(MainFragment::class.toString(), saved.toString())
+        }
+
         val cameraButton: ImageButton = view.findViewById(R.id.camera_button)
         cameraButton.setOnClickListener {
-            captureImage()
+            val activity = requireActivity()
+            val file = File(activity.filesDir,"images/go_capture.png")
+            val imageUri = FileProvider.getUriForFile(
+                activity,
+                BuildConfig.APPLICATION_ID + ".images.provider",
+                file
+            )
+            takePicture.launch(imageUri)
         }
 
         val imageButton: ImageButton = view.findViewById(R.id.image_button)
         imageButton.setOnClickListener {
-            loadImage()
+            getContent.launch("image/*")
         }
 
         // Inflate the layout for this fragment
         return view
-    }
-
-    private fun captureImage() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivity(intent)
-    }
-
-    private fun loadImage() {
-        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        startActivity(intent)
     }
 }

2d75d1ad0e33f806b1beebc4c0ad82bb84491795
Code-Apprentice <codeguru42@gmail.com>
2023-01-08 10:08:19 -0700

    onClickListener for upload button

--- app/src/main/java/codeguru/gocapture/ImageFragment.kt
+++ app/src/main/java/codeguru/gocapture/ImageFragment.kt
@@ -2,12 +2,13 @@ package codeguru.gocapture
 
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
 
 class ImageFragment : Fragment() {
     val args: ImageFragmentArgs by navArgs()
@@ -19,7 +20,18 @@ class ImageFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_image, container, false)
         val imageView = view.findViewById<ImageView>(R.id.image_view)
-        imageView.setImageURI(Uri.parse(args.imageUri))
+        val imageUri = Uri.parse(args.imageUri)
+        imageView.setImageURI(imageUri)
+
+        val uploadButton = view.findViewById<ImageView>(R.id.upload_button)
+        uploadButton.setOnClickListener {
+            processImage(it)
+        }
+
         return view
+    }
+
+    private fun processImage(view: View) {
+        Snackbar.make(view, "Processing Image", Snackbar.LENGTH_LONG).show()
     }
 }

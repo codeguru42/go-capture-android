177d00846a70900317d2a86c72e5f24d7c50fe0b
Code-Apprentice <codeguru42@gmail.com>
2023-01-08 10:19:27 -0700

    Make POST method accept multipart

--- app/src/main/java/codeguru/gocapture/GoCaptureService.kt
+++ app/src/main/java/codeguru/gocapture/GoCaptureService.kt
@@ -1,10 +1,12 @@
 package codeguru.gocapture
 
 import okhttp3.MultipartBody
+import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
 
 interface GoCaptureService {
+    @Multipart
     @POST("capture")
     fun captureImage(@Part image: MultipartBody.Part)
 }

e4971f6aa307944a70c8979514242af25be2d780
Code-Apprentice <codeguru42@gmail.com>
2023-01-08 11:29:38 -0700

    POST image to API

--- app/src/main/java/codeguru/gocapture/ImageFragment.kt
+++ app/src/main/java/codeguru/gocapture/ImageFragment.kt
@@ -2,13 +2,22 @@ package codeguru.gocapture
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+
 
 class ImageFragment : Fragment() {
     val args: ImageFragmentArgs by navArgs()
@@ -23,15 +32,35 @@ class ImageFragment : Fragment() {
         val imageUri = Uri.parse(args.imageUri)
         imageView.setImageURI(imageUri)
 
-        val uploadButton = view.findViewById<ImageView>(R.id.upload_button)
+        val uploadButton = view.findViewById<FloatingActionButton>(R.id.upload_button)
         uploadButton.setOnClickListener {
-            processImage(it)
+            processImage(imageUri)
         }
 
         return view
     }
 
-    private fun processImage(view: View) {
-        Snackbar.make(view, "Processing Image", Snackbar.LENGTH_LONG).show()
+    private fun processImage(imageUri: Uri) {
+        val inputStream = requireActivity().contentResolver.openInputStream(imageUri)
+        val filePart = MultipartBody.Part.createFormData(
+            "image",
+            imageUri.lastPathSegment,
+            RequestBody.create(MediaType.parse("image/*"), inputStream?.readBytes())
+        )
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://192.168.1.2:8000/")
+            .build()
+        val service = retrofit.create(GoCaptureService::class.java)
+        val call = service.captureImage(filePart)
+        call.enqueue(object : Callback<Void> {
+            override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                Log.d("GoCapture", response.toString())
+            }
+
+            override fun onFailure(call: Call<Void>, t: Throwable) {
+                Log.d("GoCapture", t.toString())
+            }
+        })
+        inputStream?.close()
     }
 }

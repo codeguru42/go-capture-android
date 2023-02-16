605516904936d8edafbdddf40aaaf923a60292b0
Code-Apprentice <codeguru42@gmail.com>
2023-02-15 21:07:10 -0700

    Use views from binding object

--- app/src/main/java/codeguru/gocapture/ImageFragment.kt
+++ app/src/main/java/codeguru/gocapture/ImageFragment.kt
@@ -5,12 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import codeguru.gocapture.databinding.FragmentImageBinding
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class ImageFragment : Fragment() {
@@ -25,11 +23,11 @@ class ImageFragment : Fragment() {
         val binding = FragmentImageBinding.inflate(inflater, container, false)
         val view = binding.root
         val repository = GoCaptureRepository(requireActivity())
-        val imageView = view.findViewById<ImageView>(R.id.image_view)
+        val imageView = binding.imageView
         val imageUri = Uri.parse(args.imageUri)
         imageView.setImageURI(imageUri)
 
-        val uploadButton = view.findViewById<FloatingActionButton>(R.id.upload_button)
+        val uploadButton = binding.uploadButton
         val processingView = view.findViewById<ConstraintLayout>(R.id.processing_view)
         processingView.visibility = View.GONE
         uploadButton.setOnClickListener {

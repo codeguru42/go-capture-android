6634354657406eb8a8f6fb15b03dad978a735e34
Code-Apprentice <codeguru42@gmail.com>
2023-02-16 18:55:04 -0700

    Add id for included view

--- app/src/main/java/codeguru/gocapture/ImageFragment.kt
+++ app/src/main/java/codeguru/gocapture/ImageFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import codeguru.gocapture.databinding.FragmentImageBinding
@@ -25,7 +24,7 @@ class ImageFragment : Fragment() {
         val imageUri = Uri.parse(args.imageUri)
         binding.imageView.setImageURI(imageUri)
 
-        val processingView = binding.root.findViewById<ConstraintLayout>(R.id.processing_view)
+        val processingView = binding.processing.processingView
         processingView.visibility = View.GONE
         binding.uploadButton.setOnClickListener {
             processingView.visibility = View.VISIBLE

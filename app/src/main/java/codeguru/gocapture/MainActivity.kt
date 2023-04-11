2168bad2d90a308c30462d2cf0d03af7c9cc00b5
Code-Apprentice <codeguru42@gmail.com>
2023-04-10 22:13:10 -0600

    Use listener to get registration token

--- app/src/main/java/codeguru/gocapture/MainActivity.kt
+++ app/src/main/java/codeguru/gocapture/MainActivity.kt
@@ -1,11 +1,29 @@
 package codeguru.gocapture
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String = javaClass.name
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            Log.d(TAG, token)
+        })
     }
 }

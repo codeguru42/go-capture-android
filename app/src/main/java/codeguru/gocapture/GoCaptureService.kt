4a0f9cb782fc388c183264cedd287850c9bdf918
Code-Apprentice <codeguru42@gmail.com>
2023-04-11 19:26:04 -0600

    Send image to async endpoint

--- app/src/main/java/codeguru/gocapture/GoCaptureService.kt
+++ app/src/main/java/codeguru/gocapture/GoCaptureService.kt
@@ -1,6 +1,7 @@
 package codeguru.gocapture
 
 import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.Multipart
@@ -14,5 +15,5 @@ interface GoCaptureService {
 
     @Multipart
     @POST("capture_async/")
-    suspend fun captureImageAsync(@Part image: MultipartBody.Part, @Part("fcm_registration_token") fcmRegistrationToken: String): Response<ResponseBody>
+    suspend fun captureImageAsync(@Part image: MultipartBody.Part, @Part("fcm_registration_token") fcmRegistrationToken: RequestBody): Response<ResponseBody>
 }

21bc98aa248672943b0703b741443addf02b4ee9
Code-Apprentice <codeguru42@gmail.com>
2023-07-25 22:07:25 -0600

    Move composables to MainActivity

--- app/src/main/java/codeguru/gocapture/MainActivity.kt
+++ app/src/main/java/codeguru/gocapture/MainActivity.kt
@@ -6,9 +6,32 @@ import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material.icons.filled.PhotoCamera
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.core.content.FileProvider
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import codeguru.gocapture.ui.theme.GoCaptureTheme
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     private val TAG: String = javaClass.name
@@ -16,7 +39,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            GoCaptureTheme {
+                App(modifier = Modifier.fillMaxSize())
+            }
+        }
+
         createNotificationChannel()
 
         FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
@@ -52,5 +80,79 @@ class MainActivity : AppCompatActivity() {
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
         }
+    }
+}
+
+@Composable
+fun App(modifier: Modifier) {
+    val navController = rememberNavController()
+
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.weight(1.0f))
+            CameraButton(navController)
+            Spacer(modifier = Modifier.weight(1.0f))
+            ImageButton(navController)
+            Spacer(modifier = Modifier.weight(1.0f))
+        }
+    }
+}
+
+@Composable
+private fun ImageButton(navController: NavController) {
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = {
+            Log.d(MainFragment::class.toString(), "get content result")
+            Log.d(MainFragment::class.toString(), it.toString())
+            if (it != null) {
+                val action = MainFragmentDirections.actionImage(it.toString())
+                navController.navigate(action)
+            }
+        }
+    )
+
+    Button(onClick = { launcher.launch("image/*") }) {
+        Icon(
+            Icons.Filled.Image,
+            contentDescription = stringResource(id = R.string.load_image_button)
+        )
+    }
+}
+
+@Composable
+private fun CameraButton(navController: NavController) {
+    val imagesDir = File(LocalContext.current.filesDir, "images")
+    if (!imagesDir.exists()) {
+        imagesDir.mkdir()
+    }
+    val file = File(imagesDir, "go_capture.png")
+    val imageUri = FileProvider.getUriForFile(
+        LocalContext.current,
+        BuildConfig.APPLICATION_ID + ".images.provider",
+        file
+    )
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.TakePicture(),
+        onResult = {
+            Log.d(MainFragment::class.toString(), "take picture result")
+            Log.d(MainFragment::class.toString(), it.toString())
+            if (it) {
+                val action = MainFragmentDirections.actionImage(imageUri.toString())
+                navController.navigate(action)
+            }
+        }
+    )
+
+    Button(onClick = { launcher.launch(imageUri) }) {
+        Icon(
+            Icons.Filled.PhotoCamera,
+            contentDescription = stringResource(id = R.string.capture_image_button)
+        )
     }
 }

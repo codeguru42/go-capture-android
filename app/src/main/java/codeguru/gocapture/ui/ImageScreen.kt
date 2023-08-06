ba36d477841a71283b8b35b04aa5c2dc6c174be6
Code-Apprentice <codeguru42@gmail.com>
2023-08-06 17:45:09 -0600

    Add upload button

--- app/src/main/java/codeguru/gocapture/ui/ImageScreen.kt
+++ app/src/main/java/codeguru/gocapture/ui/ImageScreen.kt
@@ -1,5 +1,12 @@
 package codeguru.gocapture.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Upload
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -9,8 +16,24 @@ import coil.compose.AsyncImage
 
 @Composable
 fun ImageScreen(navController: NavHostController, modifier: Modifier, imageUri: String?) {
-    AsyncImage(
-        model = imageUri,
-        contentDescription = stringResource(id = R.string.loaded_image)
-    )
+    Scaffold(
+        modifier = modifier,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { }
+            ) {
+                Icon(
+                    Icons.Filled.Upload,
+                    stringResource(id = R.string.upload_image)
+                )
+            }
+        }
+    ) { contentPadding ->
+        Box(modifier = Modifier.padding(contentPadding)) {
+            AsyncImage(
+                model = imageUri,
+                contentDescription = stringResource(id = R.string.loaded_image)
+            )
+        }
+    }
 }
